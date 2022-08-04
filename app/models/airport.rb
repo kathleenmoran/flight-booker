7f633af3c5afaa795d5c0e69d95098889ec5ca56
Kathleen Moran <kathleenmoran10@gmail.com>
2022-08-04 17:37:07 -0400

    Add airport and flight models

---
+++ app/models/airport.rb
@@ -0,0 +1,6 @@
+class Airport < ApplicationRecord
+  validates :code, presence: true, length: { is: 3 }
+
+  has_many :departure_flights, class_name: 'Flight', foreign_key: 'departure_airport_id', inverse_of: :departure_airport
+  has_many :arrival_flights, class_name: 'Flight', foreign_key: 'arrival_airport_id', inverse_of: :arrival_airport
+end
---
+++ app/models/flight.rb
@@ -0,0 +1,7 @@
+class Flight < ApplicationRecord
+  validates :departure_time, presence: true
+  validates :duration_in_min, presence: true, length: { minimum: 1 }
+
+  belongs_to :departure_airport, class_name: 'Airport', inverse_of: :departure_flights
+  belongs_to :arrival_airport, class_name: 'Airport', inverse_of: :arrival_flights
+end
